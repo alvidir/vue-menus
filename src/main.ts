89d675cc1f6f3c86775cbf910cc874b5a8223038
Hector <thehector1593@gmail.com>
2023-01-03 13:07:21 +0100

    feat: add context menu component

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,12 @@
 import { App } from "vue";
 import ContextMenu from "./ContextMenu.vue";
 import { include as includeDirectives } from "vue-directives/src/main";
-import { include as includeCards } from "vue-cards/src/main";
 
 const CONTEXT_MENU_TAG = "context-menu";
 
 function include(app: App): App {
-  includeDirectives(app);
-  includeCards(app);
-
-  return app.component(CONTEXT_MENU_TAG, ContextMenu);
+  return includeDirectives(app).component(CONTEXT_MENU_TAG, ContextMenu);
 }
 
-export default {
-  ContextMenu,
-};
-
-export { include, CONTEXT_MENU_TAG };
+export default include;
+export { ContextMenu };
